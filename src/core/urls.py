f446ff267b00622afc535afc4eb1e834368aaf0e
sonu chauhan <sonuchauhan.coder@gmail.com>
2024-10-11 15:41:44 +0530

    day 3 complete

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
-from django.urls import path
-from myauth import views as authi
+from django.urls import path,include
+from user  import views as s
+
+
 
 urlpatterns = [
-    path('',authi.register,name="register"),
-    path('login',authi.login,name="login"),
     path('admin/', admin.site.urls),
+    path('user/',include("user.urls")),
+    path("",s.index)
 ]
--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from user.models import User
 
-# Create your views here.
+def createUser(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        city=request.POST.get("city")
+        print(name,email,city,password)
+
+        userobject=User()
+        userobject.name=name
+        userobject.email=email
+        userobject.password=password
+        userobject.city=city
+        userobject.save()
+        return redirect("/")
+    return render(request,"createuser.html")
+
+def index(request):
+    return render(request,"index.html")
